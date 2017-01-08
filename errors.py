725591fab0a381c2c70ddbdbecd840b49ccbb5d5
DeepikaRavi <deepikanr4@gmail.com>
2017-01-08 13:32:34 +0530

    Error classes for User Login API:  -->UserNotFound.

--- errors.py
+++ errors.py
@@ -10,26 +10,38 @@ class DBInsertError(HTTPException):
     pass
 class DBQueryError(HTTPException):
     pass
+class UserNotFound(HTTPException):
+    pass
 
 CUSTOM_ERRORS = {
     "NameEmptyError": {
         "message" : "Name cannot be empty.",
+        "success" : "false",
         "status" : 401,
     },
     "InvalidPhoneNumberError": {
         "message": "Phone number doesn't contain 10 digits. Please verify and re-enter!",
+        "success" : "false",
         "status": 401,
     },
     "EmailEmptyError": {
         "message": "Email cannot be empty. Please fill it up.",
+        "success" : "false",
         "status": 401,
     },
     "DBInsertError": {
         "message": "Unable to insert into database.",
+        "success" : "false",
         "status": 402,
     },
     "DBQueryError": {
         "message": "Unable to query user from database",
+        "success" : "false",
         "status": 402,
+    },
+    "UserNotFound": {
+        "message": "Specified User entry and registered phone number not found in database. Try the Signup option to register the same.",
+        "success" : "false",
+        "status": 403,
     },
 }
